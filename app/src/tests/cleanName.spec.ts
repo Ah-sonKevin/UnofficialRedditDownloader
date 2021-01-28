34faf170c86cc8bc02a75559a9e928173cd303ae
Kevin Ah-son <ahsonkevin@gmail.com>
2021-01-28 12:46:09 +0100

    Unit testing setup

---
+++ app/src/tests/cleanName.spec.ts
@@ -0,0 +1,5 @@
+describe("Get name function", () => {
+  test("Clean not alphanumeric character", () => {});
+  test("Don\t terminate by '_'", () => {});
+  test("No successive '_'", () => {});
+});
