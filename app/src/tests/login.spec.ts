335e31fc2b57efe46d170ea6752f9c7202b95e6e
Kevin Ah-son <ahsonkevin@gmail.com>
2021-02-11 22:35:22 +0100

    mockRouter in jest tests

--- app/src/tests/login.spec.ts
+++ app/src/tests/login.spec.ts
@@ -1,9 +1,16 @@
-import Login from "@/views/Login.vue";
+import router from "@/router";
 import { mount } from "@vue/test-utils";
 import ElementPlus from "element-plus";
 
+const App = {
+	template: `
+	  <router-link to="/posts">Go to posts</router-link>
+	  <router-view />
+	`,
+};
+
 describe("Login.vue", () => {
-	const wrapper = mount(Login, { global: { plugins: [ElementPlus] } });
+	const wrapper = mount(App, { global: { plugins: [ElementPlus, router] } });
 
 	test("Exist", () => {
 		expect(wrapper.exists()).toBeTruthy();
--- app/src/tests/manager.spec.ts
+++ app/src/tests/manager.spec.ts
@@ -2,8 +2,14 @@ import Manager from "@/views/Manager.vue";
 import { mount } from "@vue/test-utils";
 import ElementPlus from "element-plus";
 
+const mockRouter = {
+	push: jest.fn(),
+};
+
 describe("Manager.vue", () => {
-	const wrapper = mount(Manager, { global: { plugins: [ElementPlus] } });
+	const wrapper = mount(Manager, {
+		global: { plugins: [ElementPlus], mocks: { $router: mockRouter } },
+	});
 
 	test("Exist", () => {
 		expect(wrapper.exists()).toBeTruthy();
