531bf8567cf9cd7b28a875d2973b9ec51be763d2
Kevin Ah-son <ahsonkevin@gmail.com>
2021-02-23 17:42:10 +0100

    add timer for notification

--- app/src/helper/notifierHelper.ts
+++ app/src/helper/notifierHelper.ts
@@ -1,10 +1,12 @@
 import { ElMessage } from "element-plus";
 
+const DURATION = 5000;
+
 export function notify(msg: string): void {
-	ElMessage.info(msg);
+	ElMessage({ message: msg, type: "info", duration: DURATION });
 }
 
 export function notifyError(msg: string): void {
-	ElMessage.error(msg);
+	ElMessage({ message: msg, type: "error", duration: DURATION });
 }
 // todo handle limit 601/call api / s
