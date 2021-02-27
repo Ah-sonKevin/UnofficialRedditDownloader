f0d0d346b397bd0da224aeb0bed196e224a54a80
Kevin Ah-son <ahsonkevin@gmail.com>
2021-02-28 00:40:56 +0100

    simplify download function

--- api/src/downloader.ts
+++ api/src/downloader.ts
@@ -1,12 +1,12 @@
-// eslint-disable-next-line eslint-comments/disable-enable-pair
-/* eslint-disable max-lines-per-function */
 import ffmpeg from "fluent-ffmpeg";
 import { createReadStream, createWriteStream, unlink } from "fs";
 import fetch from "node-fetch";
+import Stream from "stream";
 import {
 	isMultiChannel,
 	isOneChannel,
 	ItemInfo,
+	MultiChannelItemInfo,
 	OneChannelItemInfo,
 } from "./interface/itemInfo";
 
@@ -30,127 +30,143 @@ export function cleanString(text: string): string {
 		.replace(/^_/, "")
 		.replace(/_$/, "");
 }
-
+// eslint-disable-next-line max-lines-per-function
 export async function youtubeDlDownload(
 	itemInfo: ItemInfo,
 ): Promise<NodeJS.ReadableStream> {
+	function streamMerge(
+		{
+			videoExt,
+			videoNameFile,
+			audioNameFile,
+		}: { videoExt: string; videoNameFile: string; audioNameFile: string },
+		resolve: (arg0: Stream.Readable) => void,
+		reject: (arg0: Error) => void,
+	) {
+		const nameFile = `${BASE_PATH}${itemInfo.name}.${videoExt}`;
+		const clean = () => {
+			unlink(videoNameFile, () => {});
+			unlink(audioNameFile, () => {});
+		};
+		ffmpeg()
+			.addInput(videoNameFile)
+			.addInput(audioNameFile)
+			.audioCodec("copy")
+			.videoCodec("copy")
+			.saveToFile(nameFile) // cant directly output stream for mp4 format
+			.on("error", (err: Error) => {
+				clean();
+				reject(err);
+			})
+			.on("end", () => {
+				clean();
+				const stream: Stream.Readable = createReadStream(nameFile)
+					.on("error", () => {
+						unlink(nameFile, () => {});
+						reject(new Error()); // todo check empty error
+						stream.destroy();
+					})
+					.on("close", () => {
+						unlink(nameFile, () => {});
+					})
+					.on("open", () => {
+						stream.pause();
+						resolve(stream);
+					});
+			});
+	}
+	function makeName(name: string, ext: string, type: "video" | "audio") {
+		return `${BASE_PATH}${cleanString(name)}_${type}.${cleanString(ext)}`;
+	}
+
+	function downloadChannel( // tocheck promisify ?
+		mediaStream: NodeJS.ReadableStream,
+		mediaName: string,
+		callback: () => void,
+	) {
+		Stream.pipeline(mediaStream, createWriteStream(mediaName), (err) => {
+			if (err) {
+				throw err;
+			} else {
+				callback();
+			}
+		});
+	}
 	// eslint-disable-next-line max-statements
-	return new Promise((resolve, reject) => {
+	async function multiChannelDownload(
+		multiItemInfo: MultiChannelItemInfo,
+	): Promise<NodeJS.ReadableStream> {
+		const videoExt: string = multiItemInfo.video.ext;
+		const videoNameFile = makeName(multiItemInfo.name, videoExt, "video");
+
+		const audioNameFile = makeName(
+			multiItemInfo.name,
+			multiItemInfo.audio.ext,
+			"audio",
+		);
+
 		let videoStreamDone = false;
 		let audioStreamDone = false;
-		let audioExt = "";
-		let videoExt = "";
-		let audioNameFile = "";
-		let videoNameFile = "";
-
-		function streamMerge() {
-			if (videoStreamDone && audioStreamDone) {
-				const nameFile = `${BASE_PATH}${itemInfo.name}.${videoExt}`;
-				const clean = () => {
-					unlink(videoNameFile, () => {});
-					unlink(audioNameFile, () => {});
-				};
-				ffmpeg()
-					.addInput(videoNameFile)
-					.addInput(audioNameFile)
-					.audioCodec("copy")
-					.videoCodec("copy")
-					.saveToFile(nameFile) // cant directly output stream for mp4 format
-					.on("error", (err: Error) => {
-						clean();
-						reject(err);
-					})
-					.on("end", () => {
-						clean();
-						const stream = createReadStream(nameFile)
-							.on("error", () => {
-								unlink(nameFile, () => {});
-								reject();
-								stream.destroy();
-							})
-							.on("close", () => {
-								unlink(nameFile, () => {});
-							})
-							.on("open", () => {
-								resolve(stream.pause());
-							});
+		const respVideo = await fetch(multiItemInfo.video.url);
+		if (respVideo.ok) {
+			const respAudio = await fetch(multiItemInfo.audio.url);
+			if (respAudio.ok) {
+				return new Promise((resolve, reject) => {
+					downloadChannel(respVideo.body, videoNameFile, () => {
+						videoStreamDone = true;
+						if (videoStreamDone && audioStreamDone) {
+							streamMerge(
+								{ videoExt, videoNameFile, audioNameFile },
+								resolve,
+								reject,
+							);
+						}
 					});
-			}
-		}
 
-		if (isOneChannel(itemInfo)) {
-			fetch(itemInfo.url)
-				.then((res) => {
-					if (res.ok) {
-						resolve(res.body);
-					} else {
-						reject();
-					}
-					return 0; // tocheck
-				})
-				.catch((err: Error) => reject(err));
-		} else if (isMultiChannel(itemInfo)) {
-			videoExt = itemInfo.video.ext;
-			audioExt = itemInfo.audio.ext;
-			videoNameFile = `${BASE_PATH}${cleanString(
-				itemInfo.name,
-			)}_video.${cleanString(videoExt)}`;
-			audioNameFile = `${BASE_PATH}${cleanString(
-				itemInfo.name,
-			)}_audio.${cleanString(audioExt)}`;
-
-			fetch(itemInfo.video.url)
-				.then((respVideo) => {
-					// eslint-disable-next-line promise/always-return
-					if (respVideo.ok) {
-						// eslint-disable-next-line promise/no-nesting
-						fetch(itemInfo.audio.url)
-							.then((respAudio) => {
-								if (respAudio) {
-									respVideo.body
-										.pipe(
-											createWriteStream(videoNameFile)
-												.on("finish", () => {
-													videoStreamDone = true;
-													streamMerge();
-												})
-												.on("error", (err: Error) => {
-													reject(err);
-												}),
-										)
-										.on("error", (err: Error) => {
-											reject(err);
-										});
-
-									respAudio.body
-										.pipe(
-											createWriteStream(audioNameFile)
-												.on("finish", () => {
-													audioStreamDone = true;
-													streamMerge();
-												})
-												.on("error", (err: Error) => {
-													reject(err);
-												}),
-										)
-										.on("error", (err: Error) => {
-											reject(err);
-										});
-								} else {
-									reject();
+					Stream.pipeline(
+						respAudio.body,
+						createWriteStream(audioNameFile),
+						(err) => {
+							if (err) {
+								throw err;
+							} else {
+								audioStreamDone = true;
+								if (videoStreamDone && audioStreamDone) {
+									streamMerge(
+										{ videoExt, videoNameFile, audioNameFile },
+										resolve,
+										reject,
+									);
 								}
-								return 0; // tocheck
-							})
-							.catch((err: Error) => reject(err));
-					} else {
-						reject();
-					}
-				})
-				.catch((err: Error) => reject(err));
-		} else {
-			throw new Error("Invalid item info");
+							}
+						},
+					);
+				});
+			}
+			throw new Error();
 		}
-	});
+		throw new Error();
+	}
+
+	async function oneChannelDownload(oneChannelInfo: OneChannelItemInfo) {
+		const res = await fetch(oneChannelInfo.url);
+		if (res.ok) {
+			return res.body.pause(); // tocheck
+		}
+		throw new Error();
+	}
+
+	async function downloadItem(): Promise<NodeJS.ReadableStream> {
+		if (isOneChannel(itemInfo)) {
+			return oneChannelDownload(itemInfo);
+		}
+		if (isMultiChannel(itemInfo)) {
+			return multiChannelDownload(itemInfo);
+		}
+		throw new Error("Invalid item info");
+	}
+
+	return downloadItem();
 }
 // todo replace throw with next (auto logServer in next)
 export function downloader(itemInfo: ItemInfo): Promise<NodeJS.ReadableStream> {
