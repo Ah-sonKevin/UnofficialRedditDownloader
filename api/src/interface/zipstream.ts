964fc56d1d1c96c26429c0ba5b6c7d50c8a112f6
Kevin Ah-son <ahsonkevin@gmail.com>
2021-03-05 03:10:11 +0100

    add zipStream interface

---
+++ api/src/interface/zipstream.ts
@@ -0,0 +1,11 @@
+export interface ZipStream {
+	finish(): void;
+	pause(): ZipStream;
+	on(trigger: string, callback: (arg?: any) => void): ZipStream;
+	entry(
+		stream: NodeJS.ReadableStream,
+		{ name }: { name: string },
+		callback: (err: Error) => void,
+	): void;
+	resume(): ZipStream;
+}
