f74133c9b20d3089a2d03626c78bfaade3132e23
Kevin Ah-son <ahsonkevin@gmail.com>
2021-03-05 01:49:56 +0100

    simplify module's function

--- app/src/helper/Download/mediaDownloader.ts
+++ app/src/helper/Download/mediaDownloader.ts
@@ -12,7 +12,6 @@ import { fetchMedia } from "../fetchHelper/fetchHelper";
 import { notify } from "../notifierHelper";
 import {
 	cancelController,
-	exhaustivenessCheck,
 	getExt,
 	getSizeInfo,
 	updateDownloadSpinner,
@@ -20,43 +19,15 @@ import {
 
 const SPINNER_UPDATE_FREQUENCY = 1000;
 
-export function getItemInfo(item: SavedContentType): BatchItem {
-	if (item.isGallery) {
-		throw Error("Need Batch Download");
-	}
-	if (isText(item) || isLink(item)) {
-		// todo why use text here ?
-
-		return {
-			url: "item.externalUrl",
-			name: cleanString(item.title),
-			//	ext: "txt",
-			folder: "",
-			needYtDl: false,
-		};
-	}
-	// todo factorize
-	if (isImage(item) || isGallery(item)) {
-		return {
-			url: item.image.imageLink, // factorize : function getUrl
-			name: cleanString(item.title),
-			//	ext: "txt",
-			folder: "",
-			needYtDl: false,
-		};
-	}
-	if (isVideo(item)) {
-		return {
-			url: item.video.externalUrl,
-			name: cleanString(item.title),
-			//	ext: item.externalUrl.split(".").slice(-1)[0],
-			folder: "",
-			needYtDl: item.video.needYtDl,
-		};
-	}
-	return exhaustivenessCheck();
+export function getItemInfo(
+	item: ISavedImagePost | ISavedVideoPost,
+): RedditItem {
+	return {
+		url: item.getMediaUrl(),
+		name: item.title,
+		needYtdl: isVideo(item) ? item.video.needYtDl : false,
+	};
 }
-
 export async function downloadMedia(
 	item: SavedContentType,
 ): Promise<{ blob: Blob; name: string }> {
@@ -69,80 +40,83 @@ export async function downloadMedia(
 
 	try {
 		const x = await fetchMedia(
-			{ url: itemInfo.url, needYtDl: itemInfo.needYtDl },
+			{ url: itemInfo.url, needYtdl: itemInfo.needYtdl },
 			cancelController.signal,
 		);
 
 		const data = await fetchData(x, downloadIndicator);
-		const ext: string = getExt(item) ?? x.headers.get("MediaFormat") ?? ""; // todo check server ??
+		const ext: string = getExt(item) ?? x.headers.get("MediaFormat") ?? "";
 		return { blob: data, name: `${itemInfo.name}.${ext}` };
 	} catch (err) {
 		if ((err as Error).name === "AbortError") {
 			downloadIndicator.close();
 			notify("Download has been Canceled");
-			throw new Error();
-			// tocheck manage error
 		} else {
 			throw err;
 		}
 	}
 }
 
-// todo remove type from savedContent
 // eslint-disable-next-line max-statements
 export async function fetchData(
 	x: Response,
 	downloadIndicator: ILoadingInstance,
 ): Promise<Blob> {
-	if (x.ok) {
-		// todo add setup spinneer function / need to undersand setInterval argument
-		const tmpSize = x.headers.get("MediaSize");
-		downloadIndicator.setText("Downloading ...");
-		const fileChunks: Uint8Array[] = [];
-		let receivedData = 0;
-
-		let updateSpinner: NodeJS.Timeout;
-		if (tmpSize) {
-			const totalSize = parseInt(tmpSize);
-			const sizeInfo = getSizeInfo(totalSize);
-			updateSpinner = setInterval(() => {
-				updateDownloadSpinner(downloadIndicator, receivedData, sizeInfo); 
-			}, SPINNER_UPDATE_FREQUENCY);
-		}
-
-		const endDownload = () => {
-			downloadIndicator.close();
-			if (updateSpinner) {
-				clearInterval(updateSpinner);
-			}
-		};
-
-		const reader = x.body?.getReader();
-		if (!reader) {
-			downloadIndicator.close();
-			throw new DownloadError(`Undefined Reader error `);
-		}
-		let reading = true;
-
-		// eslint-disable-next-line no-loops/no-loops
-		while (reading) {
-			// eslint-disable-next-line no-await-in-loop
-			const { done, value } = await reader.read();
-			if (done) {
-				reading = false;
-			} else {
-				if (!value) {
-					endDownload();
-					throw new DownloadError(`Undefined Value Error`);
-				}
-				fileChunks.push(value);
-				receivedData += value.length;
-			}
-		}
-
-		endDownload();
-		return new Blob(fileChunks);
+	if (!x.ok) {
+		downloadIndicator.close();
+		throw new DownloadError(`Undefined Response Error`);
 	}
-	downloadIndicator.close();
-	throw new DownloadError(`Undefined Response Error`);
+	const tmpSize = x.headers.get("MediaSize");
+	if (!tmpSize) {
+		downloadIndicator.close();
+		throw new DownloadError(`Undefined Response Error`);
+	}
+	downloadIndicator.setText("Downloading ...");
+	const fileChunks: Uint8Array[] = [];
+	let receivedData = 0;
+
+	let updateSpinner: NodeJS.Timeout;
+	function setSpinnerInterval(size: number): NodeJS.Timeout {
+		const totalSize = size;
+		const sizeInfo = getSizeInfo(totalSize);
+		return setInterval(() => {
+			updateDownloadSpinner(downloadIndicator, receivedData, sizeInfo);
+		}, SPINNER_UPDATE_FREQUENCY);
+	}
+	if (tmpSize) {
+		updateSpinner = setSpinnerInterval(parseInt(tmpSize));
+	}
+
+	const endDownload = () => {
+		downloadIndicator.close();
+		if (updateSpinner) {
+			clearInterval(updateSpinner);
+		}
+	};
+
+	const reader = x.body?.getReader();
+	if (!reader) {
+		downloadIndicator.close();
+		throw new DownloadError(`Undefined Reader error `);
+	}
+	let reading = true;
+
+	// eslint-disable-next-line no-loops/no-loops
+	while (reading) {
+		// eslint-disable-next-line no-await-in-loop
+		const { done, value } = await reader.read();
+		if (done) {
+			reading = false;
+		} else {
+			if (!value) {
+				endDownload();
+				throw new DownloadError(`Undefined Value Error`);
+			}
+			fileChunks.push(value);
+			receivedData += value.length;
+		}
+	}
+
+	endDownload();
+	return new Blob(fileChunks);
 }
