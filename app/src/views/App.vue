7555793fb3402256a2ed1cffeefa7ef07cbff9ce
Kevin Ah-son <ahsonkevin@gmail.com>
2021-03-05 01:31:31 +0100

    remove unused

--- app/src/views/App.vue
+++ app/src/views/App.vue
@@ -25,8 +25,6 @@ export default {
 <style lang="scss">
 #app {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
 	background-color: #030303;
 	color: white;
